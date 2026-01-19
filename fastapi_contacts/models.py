b706ab9680be6fe4c39974be59fe6ebb5abeda2d
bvbl3ks <113239848+bvbl3ks@users.noreply.github.com>
2026-01-19 02:41:56 +0100

    Update models.py

--- fastapi_contacts/models.py
+++ fastapi_contacts/models.py
@@ -1,27 +1,13 @@
-from sqlalchemy import Column, Integer, String, Date, ForeignKey
-from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import Column, Integer, String, Date
+from database import Base
 
-Base = declarative_base()
-
-# ---------------- USERS ----------------
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True, nullable=False)
-    password = Column(String, nullable=False)
-
-    contacts = relationship("Contact", back_populates="owner")
-
-# ---------------- CONTACTS ----------------
 class Contact(Base):
     __tablename__ = "contacts"
+
     id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String)
-    last_name = Column(String)
-    email = Column(String)
-    phone = Column(String)
+    first_name = Column(String, index=True)
+    last_name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    phone = Column(String, unique=True, index=True)
     birthday = Column(Date, nullable=True)
     extra = Column(String, nullable=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
-
-    owner = relationship("User", back_populates="contacts")
